c4e503bf65cfff98e82448dd108c11c201751116
NFDFLDTHRY <61224280+NFDFLDTHRY@users.noreply.github.com>
2025-03-20 15:47:05 -0800

    Delete setup_env.fish
    reset0

--- setup_env.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-if test (count $argv) -lt 1
-    echo 'Usage: ./setup_env.fish <target_dir>'
-    exit 1
-end
-set target_dir $argv[1]
-cd $target_dir
-echo "Setting up development environment in $target_dir..."
-echo 'Environment setup complete.'
